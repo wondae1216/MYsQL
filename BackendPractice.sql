77235b24af78f60724e2de0788d11b128ba55019
wondae1216 <rkddnjsekl@naver.com>
2024-10-07 09:34:16 +0900

    Update BackendPractice.sql

--- BackendPractice.sql
+++ BackendPractice.sql
@@ -2,27 +2,29 @@ USE GELATO;
 
 CREATE TABLE  IF NOT EXISTS GELATOTYPES
 (
-	GLT_CODE 		VARCHAR(4) COMMENT '상품 코드',
-	GLT_NAME 		VARCHAR(10) NOT NULL COMMENT '상품명',
-	GLT_PRICE 		VARCHAR(10) NOT NULL COMMENT '상품가격',
-    PRODUCT_NUMBER 	VARCHAR(3) COMMENT '상품갯수',
+	GLT_CODE 		 VARCHAR(4) NOT NULL COMMENT '상품 코드',
+	GLT_NAME 		 VARCHAR(10) NOT NULL COMMENT '상품명',
+	GLT_PRICE 		 VARCHAR(10) NOT NULL COMMENT '상품가격',
+    PRODUCT_NUMBER 	 VARCHAR(3) COMMENT '상품갯수',
+    ORDERABLE_STATUS VARCHAR(1) COMMENT '판매 가능 상태', 
+    
 	CONSTRAINT GLT_CODE	PRIMARY KEY (GLT_CODE)
 ) COMMENT = '상품';
 
-INSERT INTO GELATOTYPES (GLT_CODE,GLT_NAME,GLT_PRICE,PRODUCT_NUMBER) VALUES
-('G1','치즈케익젤라또','3200원','20개'),
-('G2','딥초코젤라또','3100원','20개'),
-('G3','초당젤라또','3300원','20개'),
-('G4','쿠키앤크림젤라또','3400원','20개'),
-('G5','허니요거트젤라또','3100원','20개'),
-('G6','말차젤라또','3100원','20개'),
-('G7','연유젤라또','3200원','20개'),
-('G8','딸기젤라또','3200원','20개'),
-('G9','흑임자젤라또','3400원','20개'),
-('G10','달고나젤라또','3400원','20개'),
-('G11','고구마젤라또','3100원','20개'),
-('G12','순우유젤라또','3000원','20개'),
-('G13','저칼로리젤라또','2900원','20개');
+INSERT INTO GELATOTYPES (GLT_CODE,GLT_NAME,GLT_PRICE,PRODUCT_NUMBER,ORDERABLE_STATUS) VALUES
+('G1','치즈케익젤라또','3200원','20개','Y'),
+('G2','딥초코젤라또','3100원','20개','Y'),
+('G3','초당젤라또','3300원','20개','N'),
+('G4','쿠키앤크림젤라또','3400원','20개','Y'),
+('G5','허니요거트젤라또','3100원','20개','Y'),
+('G6','말차젤라또','3100원','20개','Y'),
+('G7','연유젤라또','3200원','20개','Y'),
+('G8','딸기젤라또','3200원','20개','Y'),
+('G9','흑임자젤라또','3400원','20개','N'),
+('G10','달고나젤라또','3400원','20개','Y'),
+('G11','고구마젤라또','3100원','20개','Y'),
+('G12','순우유젤라또','3000원','20개','Y'),
+('G13','저칼로리젤라또','2900원','20개','Y');
 
 CREATE TABLE IF NOT EXISTS  STAFF
 (
